e0c685b09c27a70ef89a3805f90afaba866e445e
Pursuit <fr.pursuit@gmail.com>
2021-02-16 23:04:18 +0100

    Graph test

--- front/index.js
+++ front/index.js
@@ -9,151 +9,47 @@ $(function () {
 
   'use strict'
 
-  // Make the dashboard widgets sortable Using jquery UI
-  $('.connectedSortable').sortable({
-    placeholder         : 'sort-highlight',
-    connectWith         : '.connectedSortable',
-    handle              : '.card-header, .nav-tabs',
-    forcePlaceholderSize: true,
-    zIndex              : 999999
-  })
-  $('.connectedSortable .card-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move')
-
-  // jQuery UI sortable for the todo list
-  $('.todo-list').sortable({
-    placeholder         : 'sort-highlight',
-    handle              : '.handle',
-    forcePlaceholderSize: true,
-    zIndex              : 999999
-  })
-
-  // bootstrap WYSIHTML5 - text editor
-  $('.textarea').summernote()
-
-  $('.daterange').daterangepicker({
-    ranges   : {
-      'Today'       : [moment(), moment()],
-      'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-      'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-      'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-      'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-    },
-    startDate: moment().subtract(29, 'days'),
-    endDate  : moment()
-  }, function (start, end) {
-    window.alert('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-  })
-
   /* jQueryKnob */
   $('.knob').knob()
-
-  // jvectormap data
-  var visitorsData = {
-    'US': 398, //USA
-    'SA': 400, //Saudi Arabia
-    'CA': 1000, //Canada
-    'DE': 500, //Germany
-    'FR': 760, //France
-    'CN': 300, //China
-    'AU': 700, //Australia
-    'BR': 600, //Brazil
-    'IN': 800, //India
-    'GB': 320, //Great Britain
-    'RU': 3000 //Russia
-  }
 
   // The Calender
   $('#calendar').datetimepicker({
     format: 'L',
-    inline: true
+    inline: true,
+    maxDate: new Date(),
+    timepicker: false
   })
 
-  // SLIMSCROLL FOR CHAT WIDGET
-  $('#chat-box').overlayScrollbars({
-    height: '250px'
-  })
+  $("#calendar").on("change.datetimepicker", function (e)
+  {
+    if (e.oldDate !== e.date)
+    {
+      for(var i = 0; i < salesGraphChart.config.data.datasets[0].data.length; i++)
+        salesGraphChartData.datasets[0].data[i] = Math.random() * 10000
+      for(i = 0; i < pieData.datasets[0].data.length; i++)
+        pieData.datasets[0].data[i] = Math.random() * 10000
 
-  /* Chart.js Charts */
-  // Sales chart
-  var salesChartCanvas = document.getElementById('revenue-chart-canvas').getContext('2d');
-  //$('#revenue-chart').get(0).getContext('2d');
-
-  var salesChartData = {
-    labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label               : 'Digital Goods',
-        backgroundColor     : 'rgba(60,141,188,0.9)',
-        borderColor         : 'rgba(60,141,188,0.8)',
-        pointRadius          : false,
-        pointColor          : '#3b8bba',
-        pointStrokeColor    : 'rgba(60,141,188,1)',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(60,141,188,1)',
-        data                : [28, 48, 40, 19, 86, 27, 90]
-      },
-      {
-        label               : 'Electronics',
-        backgroundColor     : 'rgba(210, 214, 222, 1)',
-        borderColor         : 'rgba(210, 214, 222, 1)',
-        pointRadius         : false,
-        pointColor          : 'rgba(210, 214, 222, 1)',
-        pointStrokeColor    : '#c1c7d1',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data                : [65, 59, 80, 81, 56, 55, 40]
-      },
-    ]
-  }
-
-  var salesChartOptions = {
-    maintainAspectRatio : false,
-    responsive : true,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        gridLines : {
-          display : false,
-        }
-      }],
-      yAxes: [{
-        gridLines : {
-          display : false,
-        }
-      }]
+      salesGraphChart.update()
+      pieChart.update()
     }
-  }
-
-  // This will get the first returned node in the jQuery collection.
-  var salesChart = new Chart(salesChartCanvas, { 
-      type: 'line', 
-      data: salesChartData, 
-      options: salesChartOptions
-    }
-  )
+})
 
   // Donut Chart
   var pieChartCanvas = $('#sales-chart-canvas').get(0).getContext('2d')
   var pieData        = {
     labels: [
-        'Instore Sales', 
-        'Download Sales',
-        'Mail-Order Sales', 
+        'Power consumption', 
+        'Inbound Internet traffic',
+        'Outbound Internet traffic'
     ],
     datasets: [
       {
         data: [30,12,20],
-        backgroundColor : ['#f56954', '#00a65a', '#f39c12'],
+        backgroundColor : ['#edce2f', '#2fb555', '#2f97ed'],
       }
     ]
   }
   var pieOptions = {
-    legend: {
-      display: false
-    },
     maintainAspectRatio : false,
     responsive : true,
   }
@@ -170,7 +66,7 @@ $(function () {
   //$('#revenue-chart').get(0).getContext('2d');
 
   var salesGraphChartData = {
-    labels  : ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2', '2012 Q3', '2012 Q4', '2013 Q1', '2013 Q2'],
+    labels  : ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00'],
     datasets: [
       {
         label               : 'Digital Goods',
@@ -183,7 +79,7 @@ $(function () {
         pointHoverRadius    : 7,
         pointColor          : '#efefef',
         pointBackgroundColor: '#efefef',
-        data                : [2666, 2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432]
+        data                : [2666, 2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432, 2666, 2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432, 2666, 2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432]
       }
     ]
   }
