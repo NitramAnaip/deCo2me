0dcb7393f27423b76b29c9148a8729813495e4fb
Pursuit <fr.pursuit@gmail.com>
2021-02-18 23:09:33 +0100

    Windows/Unix Measure program

---
+++ back/Measure/LibreHardwareMonitorLib/Hardware/IComputer.cs
@@ -0,0 +1,37 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (C) LibreHardwareMonitor and Contributors.
+// Partial Copyright (C) Michael Möller <mmoeller@openhardwaremonitor.org> and Contributors.
+// All Rights Reserved.
+
+using System.Collections.Generic;
+
+namespace LibreHardwareMonitor.Hardware
+{
+    public delegate void HardwareEventHandler(IHardware hardware);
+
+    public interface IComputer : IElement
+    {
+        bool IsCpuEnabled { get; }
+
+        bool IsControllerEnabled { get; }
+
+        bool IsGpuEnabled { get; }
+
+        IList<IHardware> Hardware { get; }
+
+        bool IsStorageEnabled { get; }
+
+        bool IsMotherboardEnabled { get; }
+
+        bool IsNetworkEnabled { get; }
+
+        bool IsMemoryEnabled { get; }
+
+        string GetReport();
+
+        event HardwareEventHandler HardwareAdded;
+
+        event HardwareEventHandler HardwareRemoved;
+    }
+}
---
+++ back/Measure/Measure.Unix/Program.cs
@@ -0,0 +1,18 @@
+﻿using Measure.Common;
+
+namespace Measure.Unix
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string command = Helper.GetCommand(args);
+
+			if(command == "name")
+				Helper.OutputComputerName();
+			else if(command == "net")
+				Helper.OutputNetworkUpDown();
+			else Helper.OutputUnknownCommand();
+		}
+	}
+}
